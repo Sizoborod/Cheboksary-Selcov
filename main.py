272b4f246a2217c30ec2144ea7938793c1def8bf
sizoborod <se_t610@bk.ru>
2025-11-20 16:58:30 +0300

    Запустил окно

--- main.py
+++ main.py
@@ -4,13 +4,26 @@ from PyQt6.QtCore import Qt, QRectF, QPointF
 from PyQt6.QtWidgets import QWidget, QApplication
 from random import randint
 from math import sin, cos, pi
+from PyQt6 import uic
 
 
 class Suprematism(QWidget):
     def __init__(self):
         super().__init__()
-        self.initUI()
+        uic.loadUi('Ui.ui', self)  # Загружаем дизайн
+        self.pushButton.clicked.connect(self.run)
         self.do_paint = False
         self.x, self.y = -100, -100
         self.fig = 3
         self.setMouseTracking(True)
+
+    def run(self):
+        self.label.setText("OK")
+        # Имя элемента совпадает с objectName в QTDesigner
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Suprematism()
+    ex.show()
+    sys.exit(app.exec())
